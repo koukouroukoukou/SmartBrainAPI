5ee8c73f6ebe3151c6a1e22ac86bbc7b5f344299
Koukal <franckkoukal@free.fr>
2018-10-30 13:49:31 +0100

    first commit of the project

---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors');
+const knex = require('knex');
+
+const register = require('./controllers/register');
+const signin = require('./controllers/signin');
+const profile = require('./controllers/profile');
+const image = require('./controllers/image');
+
+const db = knex({
+    client: 'pg',
+    connection: {
+        host: '127.0.0.1',
+        user: 'postgres',
+        password: '1234',
+        database: 'smart-brain'
+    }
+});
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => { res.send(database.users) });
+app.post('/signin', (req, res) => { signin.handleSignin(db, bcrypt) });
+app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) });
+app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db) });
+app.put('/image', (req, res) => { image.handleImage(req, res, db) });
+app.post('/imageurl', (req, res) => { image.handleApiCall(req, res) });
+
+// const DATABASE_URL = process.env.DATABASE_URL
+app.listen(3004, () => {
+    console.log('server is running on port 3004'); /*`Server is listening on port ${DATABASE_URL}`*/
+})
